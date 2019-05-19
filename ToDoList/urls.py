12fb2d76dbf968518e3a07414da1d6cdcd163ca7
Yeji Ahn <yeppilisa@naver.com>
2019-05-20 00:31:47 +0900

    2-~Edit

--- ToDoList/urls.py
+++ ToDoList/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import listpage.views
+from django.conf.urls import include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listpage.views.index, name='index'),
+    path('lists/', include('listpage.urls')),
 ]
--- listpage/views.py
+++ listpage/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
+from .models import List
+from django.shortcuts import redirect
 
-# Create your views here.
+# # Create your views here.
 
 def index(request):
-    return render(request, 'listpage/index.html')
+    if request.method == 'GET': 
+        lists = List.objects.all()
+        return render(request, 'listpage/index.html', {'lists': lists})
+    elif request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        List.objects.create(title=title, content=content)
+        return redirect('/lists')
+
+
+def new(request):
+    return render(request, 'listpage/new.html')
+
+def show(request, id):
+    if request.method == 'GET': # show
+        list = List.objects.get(id=id)
+        return render(request, 'listpage/show.html', {'list': list})
+    elif request.method == 'POST': # update
+        list = List.objects.get(id=id)
+        title = request.POST['title']
+        content = request.POST['content']
+        list.title = title
+        list.content = content
+        list.save()
+        return redirect('/lists')
+    
+
+def delete(request, id):
+    list = List.objects.get(id=id)
+    list.delete()
+    return redirect('/lists')
+
+def edit(request, id):
+    list = List.objects.get(id=id)
+    return render(request, 'listpage/edit.html', {'list': list})
